d1080e82c8f8849e5042a68303bd5ffecf97ea50
Abhay verma <abhaychhani@gmail.com>
2025-08-21 11:05:54 +0530

    name chaged main.jsx to Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { AuthProvider } from './context/AuthContext.jsx';
 import App from './App.jsx';
 import './main.css';
-import { AuthProvider } from './context/AuthContext.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
